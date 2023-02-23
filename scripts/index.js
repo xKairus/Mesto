26b6ad08ac0a8d7577b7b076218d85b4ab894fb5
Anton Boldyrev <a.m.boldyrev@gmail.com>
2023-02-23 21:45:36 +0500

    feat: add create net card function

--- scripts/index.js
+++ scripts/index.js
@@ -16,8 +16,16 @@ const imgInput = document.querySelector('.image-input');
 const editButton = document.querySelector('.button--edit');
 const addButton = document.querySelector('.button--add');
 const closeButton = document.querySelectorAll('.button--close');
-const saveButton = document.querySelector('.button--save');
+const saveButton = document.querySelectorAll('.button--save');
 
+// ---location---
+const cardsContainer = document.querySelector('.container.location');
+
+// --- cards array---
+import { initialCards } from './initialCards.js';
+
+
+// ---functions---
 function openPopup(popup) {
     popup.classList.remove('hidden');
 }
@@ -26,36 +34,108 @@ function closePopup(popup) {
     popup.classList.add('hidden');
     nameInput.value = ''
     jobInput.value = ''
+    locInput.value = '';
+    imgInput.value = '';
 }
 
-function editProfile(e) {
-    e.preventDefault()
-    
-    if (nameInput.value == "") {
-    } else {
-        nameField.innerHTML = nameInput.value;
+function editProfile(event) {
+    event.preventDefault();
+
+    if (nameInput.value !== '') {
+        nameField.textContent = nameInput.value;
     }
-    
-    if (jobInput.value == "") {
-    } else {
-        jobField.innerHTML = jobInput.value;
+
+    if (jobInput.value !== '') {
+        jobField.textContent = jobInput.value;
     }
-    
-    closePopup(profilePopup)
+
+    closePopup(profilePopup);
 }
 
+
+
+function createCard(card) {
+    const cardElement = document.createElement('div');
+    cardElement.classList.add('location__card');
+
+    const imageElement = document.createElement('img');
+    imageElement.src = card.link;
+    imageElement.alt = card.name;
+    imageElement.classList.add('location__image');
+
+    const infoElement = document.createElement('div');
+    infoElement.classList.add('location__info');
+
+    const nameElement = document.createElement('h2');
+    nameElement.textContent = card.name;
+    nameElement.classList.add('location__name');
+
+    const likeButton = document.createElement('button');
+    likeButton.classList.add('button', 'button--like');
+
+    infoElement.appendChild(nameElement);
+    infoElement.appendChild(likeButton);
+
+    cardElement.appendChild(imageElement);
+    cardElement.appendChild(infoElement);
+
+    return cardElement;
+}
+
+initialCards.forEach((card) => {
+    const cardElement = createCard(card);
+    cardsContainer.insertBefore(cardElement, cardsContainer.firstChild);
+});
+
+function addCard(event) {
+    event.preventDefault();
+
+    const newCard = {
+        name: locInput.value,
+        link: imgInput.value
+    };
+
+    initialCards.push(newCard);
+
+    const cardElement = createCard(newCard);
+    cardsContainer.insertBefore(cardElement, cardsContainer.firstChild);
+
+    closePopup(locationPopup);
+}
+
+
+
+function saveClick(event) {
+    event.preventDefault();
+
+    if (event.target.dataset.location === 'profile') {
+        editProfile(event);
+    } else if (event.target.dataset.location === 'card') {
+        addCard(event);
+    }
+
+    closePopup(profilePopup);
+}
+
+
+// ---open profile popup on click---
 editButton.addEventListener('click', function () {
     openPopup(profilePopup);
 });
 
+// ---open location popup on click---
 addButton.addEventListener('click', function () {
     openPopup(locationPopup);
 });
 
+// ---close opened popup on click---
 closeButton.forEach(function (button) {
     button.addEventListener('click', function () {
         closePopup(button.parentNode);
     });
 });
 
-saveButton.addEventListener('click', editProfile);
+// ---save information and close opened popup on click---
+saveButton.forEach(button => {
+    button.addEventListener('click', saveClick);
+});
