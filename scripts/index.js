49fc77d5073d8eac1ee94474ad650ae7b4e5cdf8
Anton Boldyrev <a.m.boldyrev@gmail.com>
2023-02-26 14:40:14 +0500

    feat: add functionality to validate and add new cards

--- scripts/index.js
+++ scripts/index.js
@@ -7,10 +7,10 @@ const profileEditPopup = document.querySelector('.popup--profile');
 const addCardPopup = document.querySelector('.popup--location');
 
 // ---input---
-const profileNameInput = document.querySelector('.name-input');
-const profileJobInput = document.querySelector('.job-input');
-const cardLocInput = document.querySelector('.location-input');
-const cardImgInput = document.querySelector('.image-input');
+const profileNameInput = document.querySelector('.form__input--name');
+const profileJobInput = document.querySelector('.form__input--job');
+const cardLocInput = document.querySelector('.form__input--location');
+const cardImgInput = document.querySelector('.form__input--image');
 
 // ---buttons---
 const profileEditButton = document.querySelector('.button--edit');
@@ -58,10 +58,112 @@ addCardButton.addEventListener('click', () => openPopup(addCardPopup))
 // ---close popup on click---
 const closePopup = (popup) => popup.classList.add('popup--closed')
 
-closePopupButton.forEach(function (button) {
-    button.addEventListener('click', (e) => {
-        closePopup(e.target.closest('.popup'));
+closePopupButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+        closePopup(event.target.closest('.popup'));
     });
 });
 
 
+// ---check validity of url---
+const isValidUrl = (url) => {
+    try {
+        new URL(url);
+        return true;
+    } catch (_) {
+        return false;
+    }
+};
+
+
+// ---add new card---
+const addCard = (event) => {
+    event.preventDefault();
+
+    const title = cardLocInput.value;
+    const url = cardImgInput.value;
+
+    cardLocInput.addEventListener('input', () => {
+        const errorMessage = addCardPopup.querySelector('.error-message--name');
+        errorMessage.textContent = '';
+    });
+
+    cardImgInput.addEventListener('input', () => {
+        const errorMessage = addCardPopup.querySelector('.error-message--url');
+        errorMessage.textContent = '';
+    });
+
+    if (title === '') {
+        // Show an error message to the user
+        const errorMessage = addCardPopup.querySelector('.error-message--name');
+        errorMessage.textContent = 'Введите название места';
+        return false; // Return false if the URL is not valid
+    }
+
+    if (!isValidUrl(url)) {
+        // Show an error message to the user
+        const errorMessage = addCardPopup.querySelector('.error-message--url');
+        errorMessage.textContent = 'Введите корректную ссылку на картинку';
+        return false; // Return false if the URL is not valid
+    }
+
+    // Clear any existing error message
+    const errorMessage = addCardPopup.querySelector('.error-message');
+    errorMessage.textContent = '';
+
+    renderItem(cardsContainer, title, url);
+    cardLocInput.value = '';
+    cardImgInput.value = '';
+    return true; // Return true if the card is added successfully
+}
+
+
+// ---remove card---
+const removeCard = (event) => event.target.closest('.location__card').remove();
+
+cardsContainer.addEventListener('click', (event) => {
+    if (event.target.classList.contains('button--remove')) {
+        removeCard(event);
+    }
+});
+
+
+// ---edit profile---
+const editProfile = (event) => {
+    event.preventDefault();
+
+    if (profileNameInput.value !== '') {
+        profileNameField.textContent = profileNameInput.value;
+    }
+
+    if (profileJobInput.value !== '') {
+        profileJobField.textContent = profileJobInput.value;
+    }
+
+    profileNameInput.value = '';
+    profileJobInput.value = '';
+    closePopup(profileEditPopup)
+}
+
+
+// ---submit form---
+const saveClick = (event) => {
+    event.preventDefault();
+
+    const popup = event.target.closest('.popup');
+
+    if (popup === profileEditPopup) {
+        if (editProfile(event)) {
+            closePopup(profileEditPopup);
+        }
+    } else if (popup === addCardPopup) {
+        if (addCard(event)) {
+            closePopup(addCardPopup);
+        }
+    }
+}
+
+saveFormButton.forEach(button => {
+    button.addEventListener('click', saveClick)
+    console.log('click')
+});
